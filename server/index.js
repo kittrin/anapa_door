b74dde2fcafd67afd1cfcd068d8fde11d5248db5
Кристина Щитченко <alina.andreeva202@mail.ru>
2024-01-22 14:19:35 +0300

    add alice

--- server/index.js
+++ server/index.js
@@ -1,4 +1,8 @@
-import axios from "axios";
+const {axios} =require('axios');
+const {json, send} = require('micro');
+const fetch = require('node-fetch');
+const {includes, lowerCase, random, sample} = require('lodash');
+
 
 class OpenDoorController{
     static async getOpen(){
@@ -22,5 +26,108 @@ const data = async ()=>{
 
     }
 }
-await data();
-console.log(data())
+// await data();
+// console.log(data())
+
+module.exports = async (req, res) => {
+    // Код для HTTP-ответа:
+    let statusCode = 200;
+
+    // Принимаем только POST-запросы:
+    if (req.method !== "POST") {
+        statusCode = 400;
+        await send(res, statusCode, 'Bad Request');
+        return;
+    }
+
+    // API Яндекс.Диалоги:
+    const {meta, request, session, version} = await json(req);
+
+    // Текущая сессия не закрыта:
+    let isEndSession = false;
+
+    // Получаем фразу юзера или надпись с кнопки которую он нажал (и переводим в нижний регистр):
+    const userUtterance = lowerCase(request.original_utterance);
+    console.log(userUtterance)
+    // Сообщение юзеру (на всякий случай знак пробела, чтобы застраховаться от ошибки в Яндекс.Диалоги, если соощение окажется пустой строкой):
+    let message = ' ';
+
+    // Быстрый ответ (чтобы не делать лишние запросы к стороннему API) на проверочный пинг от Яндекса:
+    if (userUtterance === 'ping') {
+        message = 'ОК';
+        isEndSession = true;
+        await send(res, statusCode, {
+            version,
+            session,
+            response: {
+                text: message,
+                end_session: isEndSession
+            }
+        });
+        return;
+    }
+
+    // Получаем массив всех слов из последней фразы юзера:
+    let userWords = [];
+
+    if (request.nlu.tokens.length > 0) {
+        const tokensArr = request.nlu.tokens;
+        for (let i = 0; i < tokensArr.length; i++) {
+            userWords.push(tokensArr[i]);
+        }
+    }
+    // Формируем перманентный вопрос к юзеру:
+    const prompt = `сейчас сделаю`;
+
+    // Теперь стараемся понять юзера. Эту логику также лучше писать в отдельном файле,
+    // поскольку может быть много кода, но в данном весьма упрощённом примере -- пишем здесь.
+    // Будем использовать функции includes() из библиотеки Lodash, которая ищет подстроку.
+
+    // Намерения юзера:
+    let intent;
+
+    const playWords = ['открой шлагбаум'];
+
+    for (let item of playWords) {
+        if (includes(userUtterance, item)) {
+            intent = 'play';
+            break;
+        }
+    }
+
+
+    // И вот он -- диалог с юзером!:
+    if (!userUtterance) {
+        if (intent === 'play') {
+            // Определение функции setData() -- в конце кода:
+            message = await setData();
+        }
+    }
+
+
+    // Ответ Алисе:
+    await send(res, statusCode, {
+        version,
+        session,
+        response: {
+            text: message,
+            end_session: isEndSession
+        }
+    });
+
+
+    // Функция, которая получает данные и возвращает отформатированную цитату:
+    async function setData() {
+        let quote;
+        let author;
+        try {
+            // Получение "сырых" данных:
+            const data = 'окей уважаемый'
+        } catch (err) {
+            quote = 'Мысль потеряна! Попробуйте ещё раз.';
+            console.error('Fail to fetch data: ' + err);
+        }
+        // Отформатированная цитата:
+        return `${quote}\n${author ? '— ' : ''}${author}\n${prompt}`;
+    }
+};
